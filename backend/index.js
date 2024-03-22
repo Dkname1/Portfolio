011148f783f6b7f9363ce7921c8c81d455c8afda
Dkname1 <115562828+Dkname1@users.noreply.github.com>
2024-03-22 11:22:20 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,7 @@ const app=express();
 const PORT=process.env.PORT
 const hostName='127.0.0.1'
 
-app.use(cors({
-     origin:["https://deploy-mern-1whq.vercel.app"],
-     methods:["POST","GET"],
-     credentials:true
-}));
+app.use(cors());
 app.use(express.json());
 app.use('/api', router)
 
